660cbd73b84d3f4e9ac6675c65a91b75c283fe6f
Njaka Rand <njaka91@gmail.com>
2024-03-03 17:07:12 +0300

    update

---
+++ src/P2C1/components/PlantItem.js
@@ -0,0 +1,25 @@
+import CareScale from './CareScale'
+import '../styles/PlantItem.css'
+
+
+function PlantItem({ id, cover, name, water, light }) {
+	return (
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+				
+			</div>
+			
+		</li>
+	
+		
+	)
+}
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
+export default PlantItem
